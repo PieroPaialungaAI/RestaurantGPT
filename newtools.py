890d997893c516a619fa11bf2073d5469ced02c1
Piero Paialunga <piero.paialunga@hotmail.com>
2025-04-20 09:56:54 -0400

    Removing the unused functions

--- newtools.py
+++ newtools.py
@@ -3,13 +3,6 @@ from constants import *
 import pandas as pd 
 
 @function_tool
-def check_wait_time(waiting_time, queue, food = None) -> str:
-    return {'waiting_time': waiting_time, queue : queue, food : food}
-    """Quiz, joke, or chat to keep a waiting customer entertained."""
-    # your actual entertainment logic goes here
-    
-
-@function_tool
 def get_menu():
     df = pd.read_csv(MENU_FILE)
     # convert to list of dicts (or JSON-serializable structure)
