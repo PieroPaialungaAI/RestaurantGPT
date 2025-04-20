ca867b40fb3e219b15457266f71473ce6f724238
Piero Paialunga <piero.paialunga@hotmail.com>
2025-04-19 22:23:27 -0400

    We are now adding first names

--- constants.py
+++ constants.py
@@ -12,3 +12,9 @@ END_TIME = 3600
 ARRIVAL_PROB = 0.33
 TICK = 60
 PAUSE = 0.5
+NAMES =  [
+    "Emma", "Liam", "Olivia", "Noah", "Ava", "William", "Sophia", "James",
+    "Isabella", "Oliver", "Mia", "Elijah", "Charlotte", "Benjamin", "Amelia",
+    "Lucas", "Harper", "Mason", "Evelyn", "Logan", "Abigail", "Ethan", "Emily",
+    "Jacob", "Elizabeth", "Michael", "Sofia", "Alexander", "Madison", "Daniel"
+]
